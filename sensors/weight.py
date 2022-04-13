8e7f5df6158b3a92f617f9d681a3e092ef95751f
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2022-04-13 23:10:24 +0300

    Update weight.py

--- sensors/weight.py
+++ sensors/weight.py
@@ -1,9 +1,6 @@
 import random
 
-'''
-As we do not have a weight sensor,
-just return a random number lol
-'''
+# As we do not have a weight sensor, just return a random number lol
 
 def calculate_weight():
     return random.randint(10, 30)
